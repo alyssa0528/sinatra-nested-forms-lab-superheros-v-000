ca19949080028abf06248bd89b1a48278389052f
Alyssa Kim <alyssa.kim@gmail.com>
2018-06-08 16:28:34 +0000

    automatically backed up by learn

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,8 +1,15 @@
 class Team
   attr_accessor :name, :motto
 
+  TEAMS = []
+
   def initialize(params)
     @name = params[:name]
     @motto = params[:motto]
   end
+
+  def self.all
+    TEAMS
+  end
+
 end
