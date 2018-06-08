a8166e3c284816107e021184de084ecd1935f753
Alyssa Kim <alyssa.kim@gmail.com>
2018-06-08 16:30:17 +0000

    automatically backed up by learn

--- app/models/superhero.rb
+++ app/models/superhero.rb
@@ -1,2 +1,16 @@
 class Superhero
-end 
+  attr_accessor :name, :power, :bio
+
+  SUPERHEROES = []
+
+  def initialize(params)
+    @name = params[:name]
+    @power = params[:power]
+    @bio = params[:bio]
+    SUPERHEROES << self
+  end
+
+  def self.all
+    SUPERHEROES
+  end
+end
